d90f5195a04d79311fb64698ca50d956a10f1a83
Saveliy Samoilov <fortnitesamsa@mail.ru>
2024-06-24 17:53:25 +0300

    z

---
+++ Samoilov/Controllers/CalculatorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Samoilov.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public String Index(int a, int b)
+        {
+            return a + " + " + b + " = " + (a + b);
+        }
+    }
+}
+//https://localhost:7124/calculator/index/1/
--- Samoilov/Program.cs
+++ Samoilov/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Calculator}/{action=Index}/{a?}/{b?}");
 
 app.Run();
