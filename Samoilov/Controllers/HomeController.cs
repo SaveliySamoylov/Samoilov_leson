fea4ca1c641a78ee03f26f7b0c70527e9d1c720b
Saveliy Samoilov <fortnitesamsa@mail.ru>
2024-06-24 18:02:50 +0300

    z

--- Samoilov/Controllers/HomeController.cs
+++ Samoilov/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Samoilov.Models;
+using Microsoft.AspNetCore.Mvc;
 using Samoilov.Models;
 using System.Diagnostics;
 
-namespace Samoilov.Controllers
+namespace Di_25.Controllers
 {
     public class HomeController : Controller
     {
@@ -13,10 +14,6 @@ namespace Samoilov.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
         public IActionResult Privacy()
         {
@@ -28,5 +25,24 @@ namespace Samoilov.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public String index(int a)
+        {
+            var answer = "";
+            for (int i = 0; i < ProductList.products.Count; i++)
+            {
+                answer += ProductList.products[i].Id;
+                answer += "\n";
+                answer += ProductList.products[i].Name;
+                answer += "\n";
+                answer += ProductList.products[i].Price;
+                answer += "\n";
+                answer += ProductList.products[i].Description;
+                answer += "\n";
+                answer += "\n";
+            }
+            return answer;
+        }
     }
 }
+//https://localhost:7124/Home/Index
---
+++ Samoilov/ProductList.cs
@@ -0,0 +1,12 @@
+﻿namespace Di_25
+{
+    public static class ProductList
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product(1, "без разницы1", 1111, "Что то там 1"),
+            new Product(2, "без разницы2", 2222, "Что то там 2"),
+            new Product(3, "без разницы3", 3333, "Что то там 3")
+        };
+    }
+}
