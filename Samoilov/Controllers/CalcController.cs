7647d7a6cddd4563c94fcae2750b46bb0d9ee1b1
Saveliy Samoilov <fortnitesamsa@mail.ru>
2024-06-24 17:58:06 +0300

    z

---
+++ Samoilov/Controllers/CalcController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Di_25.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if (c == "+")
+            {
+                return a + " + " + b + " = " + (a + b);
+            }
+            else if (c == "-")
+            {
+                return a + " - " + b + " = " + (a - b);
+            }
+            else if (c == "*")
+            {
+                return a + " * " + b + " = " + (a * b);
+            }
+            else if (c == "/")
+            {
+                return a + " / " + b + " = " + (a / b);
+            }
+            else if (c == null)
+            {
+
+                return a + " + " + b + " = " + (a + b);
+            }
+            else
+            {
+                return "Вам необходимо правильно задать операцию";
+            }
+        }
+    }
+}
+//https://localhost:7124/calc/index?a=1&b=3&c=+
